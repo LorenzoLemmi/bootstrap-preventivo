02664c40233dc0dee4fc742fc136a421d835b836
LorenzoLemmi <lorenzo.lemmi92@gmail.com>
2025-01-26 18:44:44 +0100

    feat: Add eventListener

--- js/main.js
+++ js/main.js
@@ -4,5 +4,12 @@ submitButton.addEventListener("click", function(event) {
 
     event.preventDefault();
     submitButton.disabled = true;
-    
+    const spinner = submitButton.querySelector("#spinner");
+    const status = submitButton.querySelector("#buttonStatus");
+    const originalStatusText = status.innerHTML;
+    spinner.classList.toggle("d-none");
+    status.innerHTML = `Loading...`;
+    setTimeout(() => {
+        
+    }, timeout);
 })
